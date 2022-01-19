7b3a84b6fb9ff8ccbb1e51947c04298c4be5d6bb
DeusArtifex <deus3artifex@gmail.com>
2022-01-19 19:55:00 +0100

    Basic onClick listener

--- app/src/main/java/com/example/logowanie_v2/RegisterActivity.java
+++ app/src/main/java/com/example/logowanie_v2/RegisterActivity.java
@@ -3,12 +3,27 @@ package com.example.logowanie_v2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button accept;
+    private EditText formEmail, formPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //Nasłuchuj przycisków
+        accept = findViewById(R.id.accept);
+        accept.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "Zarejestrowano", Toast.LENGTH_SHORT).show();
     }
 }
