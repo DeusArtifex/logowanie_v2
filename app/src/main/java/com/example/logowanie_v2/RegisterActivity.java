624ed63374402523035171b37d4aba8d26b456d1
DeusArtifex <deus3artifex@gmail.com>
2022-01-19 20:13:09 +0100

    make checks on register

--- app/src/main/java/com/example/logowanie_v2/RegisterActivity.java
+++ app/src/main/java/com/example/logowanie_v2/RegisterActivity.java
@@ -8,6 +8,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
     private Button accept;
     private EditText formEmail, formPass;
@@ -17,6 +20,9 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        formEmail = findViewById(R.id.formMail);
+        formPass = findViewById(R.id.formPass);
+
         //Nasłuchuj przycisków
         accept = findViewById(R.id.accept);
         accept.setOnClickListener(this);
@@ -24,6 +30,38 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
     @Override
     public void onClick(View v) {
-        Toast.makeText(this, "Zarejestrowano", Toast.LENGTH_SHORT).show();
+        String mail = formEmail.getText().toString();
+        String pass = formPass.getText().toString();
+        if(checkMail(mail) && checkPass(pass)){
+            Toast.makeText(this, "Zarejestrowano", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Błąd", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public boolean checkMail(String mail){
+        Pattern pattern = Pattern.compile("^(.*)@([a-z]{3,})\\.([a-z]{2,}$)");
+        Matcher matcher = pattern.matcher(mail);
+        if(matcher.find()){
+            return true;
+        }else {
+            formEmail.setError("Niepoprawny email");
+            Toast.makeText(this, "Błąd", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+    public boolean checkPass(String pass){
+        String strPattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{6,36}$";
+        Pattern pattern = Pattern.compile(strPattern);
+        Matcher matcher = pattern.matcher(pass);
+        if(matcher.find()){
+            return true;
+        } else{
+            formPass.setError("Wprowadź poprawne hasło");
+            Toast.makeText(this, "Błąd", Toast.LENGTH_SHORT).show();
+            return false;
+        }
     }
 }
